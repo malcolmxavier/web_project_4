4f5362749434c9113b2ddc44b7fada63a3f32434
Malcolm Xavier <malcolm.x.evans@gmail.com>
2020-08-02 12:31:39 -0400

    fix card class

--- components/Card.js
+++ components/Card.js
@@ -26,6 +26,7 @@ class Card {
 
     this._cardTrashButton.addEventListener('click', () => {
       this._cardElements.remove();
+      this._cardElements = null;
     });
 
     this._cardLikeButton.addEventListener('click', () => {
